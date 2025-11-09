0d1199926ce8f5523cf3b2b01bc224a94dd35489
Sergey Budyakin <youshtuchka@gmail.com>
2025-11-09 18:16:42 +0300

    + connect to DB

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,1 +1,12 @@
+import mongoose from 'mongoose';
+
+
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log("❌ Mongo connection error:", error.message);
+    }
+}
 
