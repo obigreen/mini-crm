26a80b64941ee4b1daf8403ae149628dee8da5f5
Sergey Budyakin <youshtuchka@gmail.com>
2025-12-11 18:36:25 +0300

    worked for db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 
 
-export async function connectDB() {
+export async function connectDB(mongoUrl) {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(mongoUrl, {
+            serverSelectionTimeoutMS: 5000,
+            maxPoolSize: 10
+        })
         console.log("Connected to MongoDB")
     } catch (error) {
         console.log("❌ Mongo connection error:", error.message);
+        throw error;
     }
 }
 
